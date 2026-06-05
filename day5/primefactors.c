57907c0138335d4263f7077af827919ebe537ded
vidhidhiman3 <25121cn122@glbitm.ac.in>
2026-06-05 14:13:53 +0530

    Create primefactors.c

---
+++ day5/primefactors.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    printf("Prime factors are %d :",n);
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            printf("%d", i);
+        }
+    
+        
+    }
+    return 0;
+}
